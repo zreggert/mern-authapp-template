c13057704f3e6290145efc44c292f0a9ef309420
Zack Eggert <zreggert@gmail.com>
2024-06-11 20:45:35 -0400

    had some information to home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-
-function About() {
+export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold  mb-4 text-slate-800'>About the project</h1>
+      <p className='mb-4 text-slate-700'>
+        I wanted to build this application so that I could have a very basic yet user friendly template for future web development projects I will be working on in the future. In this application, I have ultilized the MERN stack (MongoDB, Express.js, React.js, and Node.js) to create a function authentication app that allows new users to sign up and current users to log in, update, or delete their profiles. As an added feature, I have enabled OAuth authentication using Google OAuth so anyone with a gmail account can easily sign up.
+      </p>
+      <br />
+      <p className='mb-4 text-slate-700'>
+        For the frontend, I have used React to dynamic build the UI with pages and components and React Router to handle client side routing. Tailwaindcss is used for the minimal design and layout work but I plan to use it in the future as I found it very easy to work with and a lot less of a hassle as regular CSS.
+      </p>
+      <br />
+      <p className='mb-4 text-slate-700'>
+        Finally, the server side of application consists of a non-relational database using MongoDB to create the user model and Node.js and Express.js to handle server side routing and API calls.
+      </p>
+    </div>
   )
 }
-
-export default About
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-
-function Home() {
+export default function Home() {
   return (
-    <div>Home</div>
+    <div className='center px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold  mb-4 text-slate-800'>Welcome to my Full Stack React Authentication Application</h1>
+      <p className='mb-4 text-slate-700'>
+        A template for future applications and projects
+      </p>
+      <br />
+      <p className='mb-4 text-slate-700'>
+        Check out the About section for more info
+      </p>
+    </div>
   )
 }
-
-export default Home
